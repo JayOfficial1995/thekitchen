3cf76dd6f39a07749843110f0d1cd8087adba4e3
Jahangir Khan <jahangir.khan@emumba.com>
2020-12-14 13:15:07 +0500

    updated server name

--- application/config/database.php
+++ application/config/database.php
@@ -6,9 +6,9 @@ return [
         'concrete' => [
             'driver' => 'c5_pdo_mysql',
             'server' => 'remotemysql.com',
-            'database' => 'XYsQhvFi3B',
-            'username' => 'XYsQhvFi3B',
-            'password' => 'L7bV68y88A',
+            'database' => 'SSGG93WJay',
+            'username' => 'SSGG93WJay',
+            'password' => 'grwim1yLqW',
             'character_set' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
         ],
